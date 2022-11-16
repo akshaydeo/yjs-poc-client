a81fcf8f0f6e139a1be71e33830fca68889295e9
Akshay Deo <akshay@akshaydeo.com>
2022-11-17 02:49:22 +0530

    incremental updates even for a large file

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import * as monaco from 'monaco-editor';
 import { useCallback, useEffect, useRef, useState } from 'react';
 import io from 'socket.io-client';
 import { MonacoBinding } from 'y-monaco';
+import * as awarenessProtocol from 'y-protocols/awareness';
 import * as Y from 'yjs';
 import './App.css';
 
@@ -18,16 +19,21 @@ export default function App() {
   const [contentVector, setContentVector] = useState(undefined);
   const [update, setUpdate] = useState(undefined);
   const ydocument = new Y.Doc();
-
+  const awareness = new awarenessProtocol.Awareness(ydocument);
   const debouncedUpdate = useCallback(debounce(() => {
     const toPush = updates;
     updates = [];
+    let size = 0;
+    toPush.forEach(u => {
+      size += u.length;
+    });
+    console.log(size);    
     axios.post('http://localhost:8000/docupdate2', { updates: toPush, origin: ydocument.clientID }, {
       headers: {
         'Content-Type': 'application/json'
       }
     }).then(() => { }).catch((err) => { });
-  }, 500), []);
+  }, 500, { maxWait: 1000 }), []);
 
   useEffect(() => {
     if (contentLoaded) return;
@@ -47,18 +53,50 @@ export default function App() {
     } catch (err) {
       console.log(err);
     }
+    // awareness.on('update', ({ added, updated, removed }) => {
+    //   const changedClients = added.concat(updated).concat(removed);
+    //   const base64encoded = fromUint8Array(awarenessProtocol.encodeAwarenessUpdate(awareness, changedClients));
+    //   axios.post('http://localhost:8000/awareness', { update: base64encoded }, {
+    //     headers: {
+    //       'Content-Type': 'application/json'
+    //     }
+    //   }).then(() => { }).catch((err) => { });
+    // });
+    awareness.setLocalStateField('user', {
+      // Define a print name that should be displayed
+      name: 'Akshay deo',
+      // Define a color that should be associated to the user:
+      color: '#ffb61e' // should be a hex color
+    })
     const socket = io("ws://localhost:8001");
-    socket.on('updates', (update, origin) => {
-      console.log(update.origin, ydocument.clientID);
+    socket.on('updates', (update) => {
+      // console.log(update.origin, ydocument.clientID);
       if (update.origin === ydocument.clientID) {
-        console.log('not applying update');
+        // console.log('not applying update');
         return;
       }
       Y.applyUpdate(ydocument, toUint8Array(update.update));
     });
+    socket.on('awareness', (update) => {
+      awarenessProtocol.applyAwarenessUpdate(awareness, toUint8Array(update.update));
+    });
+    
+    const model = monaco.editor.createModel('', 'text');
+    const editor = monaco.editor.create(node.current, {});
+    editor.setModel(model);
+    const text = ydocument.getText('monaco');
+    const monacoBinding = new MonacoBinding(text, editor.getModel(), new Set([editor]), awareness);
+    return () => {
+      editor.dispose();
+      socket.disconnect();
+      monacoBinding.destroy();
+    };
+  }, [contentLoaded]);
+
+  useEffect(()=>{
     ydocument.on('update', (update) => {
-      console.log(update);
-      const base64encoded = fromUint8Array(Y.encodeStateAsUpdate(ydocument, content));
+      // console.log(update);
+      const base64encoded = fromUint8Array(update);
       updates.push(base64encoded);
       debouncedUpdate();
       // axios.post('http://localhost:8000/docupdate', { update: base64encoded, origin: ydocument.clientID }, {
@@ -67,17 +105,7 @@ export default function App() {
       //   }
       // }).then(() => { }).catch((err) => { });
     });
-    const model = monaco.editor.createModel('', 'text');
-    const editor = monaco.editor.create(node.current, {});
-    editor.setModel(model);
-    const text = ydocument.getText('monaco');
-    const monacoBinding = new MonacoBinding(text, editor.getModel(), new Set([editor]), undefined);
-    return () => {
-      editor.dispose();
-      socket.disconnect();
-      monacoBinding.destroy();
-    };
-  }, [contentLoaded]);
+  },[contentVector,ydocument]);
 
   return <div ref={node} style={{ width: '100vw', height: '100vh' }} />
 }
